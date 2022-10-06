3daf5498bf05870831d7f0e67b902416857b82a1
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-10-06 23:04:37 +0200

    working mouse tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,65 @@
+import { useRef, useState } from 'react';
 import './App.css';
 
-import React, { useState } from 'react';
-
-import logo from './logo.svg';
+type Pos = {
+  x: number;
+  y: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [posOne, setPos] = useState<Pos>();
+  const [posTwo, setPosTwo] = useState<Pos>();
+  const [dragging, setDragging] = useState(false);
+  const gameArea = useRef<SVGSVGElement>(null);
+
+  const setPosition = (e: React.MouseEvent<SVGCircleElement, MouseEvent>) => {
+    const x = e.currentTarget.cx.baseVal.value;
+    const y = e.currentTarget.cy.baseVal.value;
+    setPos({ x, y });
+  };
+
+  const updateMousePos = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    if (!dragging) {
+      return;
+    }
+
+    const area = gameArea.current;
+    if (!area) {
+      return;
+    }
+
+    const bound = area.getBoundingClientRect();
+
+    setPosTwo({
+      x: e.clientX - bound.left - area.clientLeft,
+      y: e.clientY - bound.top - area.clientTop,
+    });
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
+      <div className="gameArea">
+        <svg
+          ref={gameArea}
+          viewBox={['0', '0', 450, 400].join(' ')}
+          width="450"
+          height="400"
+          onMouseDown={() => setDragging(true)}
+          onMouseUp={() => setDragging(false)}
+          onMouseMove={updateMousePos}
+        >
+          <circle cx="150" cy="100" r="20" fill="white" onClick={setPosition} />
+          <circle cx="200" cy="200" r="20" fill="white" onClick={setPosition} />
+          <line
+            x1={posOne?.x || 50}
+            x2={posTwo?.x || 0}
+            y1={posOne?.y || 110}
+            y2={posTwo?.y || 0}
+            stroke="white"
+            strokeWidth="6"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
