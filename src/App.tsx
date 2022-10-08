bb0830810a8acc3d0c61c57ba70272f73b4f4f8a
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-10-08 18:02:45 +0200

    working layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,41 @@
-import { useRef, useState } from 'react';
 import './App.css';
 
-type Pos = {
-  x: number;
-  y: number;
+const GAME = {
+  cols: new Array(5).fill(''),
+  rows: new Array(5).fill(''),
+  boxSize: 80,
+};
+
+const Box: React.FC<{ col: number; row: number }> = ({ col, row }) => {
+  const left = col * (GAME.boxSize - 5);
+  const sideOpacity = col === 0 ? 100 : 0;
+  const topOpacity = row > 0 ? 0 : 100;
+
+  return (
+    <div className="box" style={{ left }}>
+      <div className="side line" style={{ opacity: sideOpacity, zIndex: sideOpacity }} />
+      <div className="side line" />
+      <div className="top line" style={{ opacity: topOpacity, zIndex: topOpacity }} />
+      <div className="bottom line" />
+    </div>
+  );
 };
 
 function App() {
-  const [posOne, setPos] = useState<Pos>();
-  const [posTwo, setPosTwo] = useState<Pos>();
-  const [dragging, setDragging] = useState(false);
-  const gameArea = useRef<SVGSVGElement>(null);
-
-  const setPosition = (e: React.MouseEvent<SVGCircleElement, MouseEvent>) => {
-    const x = e.currentTarget.cx.baseVal.value;
-    const y = e.currentTarget.cy.baseVal.value;
-    setPos({ x, y });
-  };
-
-  const updateMousePos = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-    if (!dragging) {
-      return;
-    }
-
-    const area = gameArea.current;
-    if (!area) {
-      return;
-    }
-
-    const bound = area.getBoundingClientRect();
-
-    setPosTwo({
-      x: e.clientX - bound.left - area.clientLeft,
-      y: e.clientY - bound.top - area.clientTop,
-    });
-  };
-
   return (
     <div className="App">
       <div className="gameArea">
-        <svg
-          ref={gameArea}
-          viewBox={['0', '0', 450, 400].join(' ')}
-          width="450"
-          height="400"
-          onMouseDown={() => setDragging(true)}
-          onMouseUp={() => setDragging(false)}
-          onMouseMove={updateMousePos}
-        >
-          <circle cx="150" cy="100" r="20" fill="white" onClick={setPosition} />
-          <circle cx="200" cy="200" r="20" fill="white" onClick={setPosition} />
-          <line
-            x1={posOne?.x || 50}
-            x2={posTwo?.x || 0}
-            y1={posOne?.y || 110}
-            y2={posTwo?.y || 0}
-            stroke="white"
-            strokeWidth="6"
-          />
-        </svg>
+        {GAME.rows.map((_, row) => (
+          <div
+            key={row}
+            className="gameRow"
+            style={{ width: GAME.boxSize * 5, top: row * (GAME.boxSize - 5) + 8 }}
+          >
+            {GAME.cols.map((_, col) => (
+              <Box key={col} col={col} row={row} />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
